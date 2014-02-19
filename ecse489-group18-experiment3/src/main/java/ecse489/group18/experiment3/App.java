04334cf31d8ffb61a3efafd8678ed62f853ac3b0
Jean-Sebastien Dery <jeansebastien.dery@gmail.com>
2014-02-18 22:18:22 -0500

    Generated the dummy Maven project to be imported in Eclipse

---
+++ ecse489-group18-experiment3/src/main/java/ecse489/group18/experiment3/App.java
@@ -0,0 +1,13 @@
+package ecse489.group18.experiment3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
